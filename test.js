b73dfeaa0377d5233f967dcb789ef136e292bc79
jinhyeok15 <ekfrhd97@gmail.com>
2021-09-17 19:10:33 +0900

    함수로 변경

--- test.js
+++ test.js
@@ -1,21 +1,18 @@
 import fetch from "node-fetch";
 
-const fetchApi = 
-fetch('http://3.37.245.109:3000/room/leagueoflegends')
-.then(res => {
-    console.log(res);
-    return res.json();
-})
-.then(json => {
-    const length = Object.keys(json.data).length;
-    console.log(Object.keys(json.data).length)
-    for (var i=0; i<length; i++) {
-        console.log(json.data[i].code);
-    }
-    // return data;
-})
-.catch(err => {
-    console.log('Fetch error', err);
+const fetchApi = () => {
+    fetch('http://3.37.245.109:3000/room/leagueoflegends')
+    .then(res => {
+        // console.log(res);
+        return res.json();
+    })
+    .then(json => {
+        // console.log(json.data)
+        console.log(json.data);
+    })
+    .catch(err => {
+        console.log('Fetch error', err);
 });
+};
 
 export default fetchApi;
